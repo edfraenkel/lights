3a671e3a8c7e1e0b51f1102461a74de4baa18a43
Daniel Fraenkel <e.d.fraenkel@gmail.com>
2015-11-26 21:29:26 +0000

    changing tabs to spaces and improving the error messaging a bit

--- NeoPixelIkea/NeoPixelIkea/CommandReader.h
+++ NeoPixelIkea/NeoPixelIkea/CommandReader.h
@@ -2,67 +2,73 @@
 
 class CommandReader {
 public:
-	CommandReader(Stream &s) : _s(s) {};
+  CommandReader(Stream &s) : _s(s) {};
 
-	template<typename A>
-	inline void _(A const &x) {
-		_s.print(x);
-	};
+  template<typename A>
+  inline void _(A const &x) {
+    _s.print(x);
+  };
 
-	template<typename A>
-	inline void _n(A const &x) {
-		_s.println(x);
-	};
+  template<typename A>
+  inline void _n(A const &x) {
+    _s.println(x);
+  };
 
-	inline bool nextIsCommand() {
-		_gotoNextToken();
-		return _s.peek() == '[';
-	}
+  inline bool nextIsCommand() {
+    _gotoNextToken();
+    return _s.peek() == '[';
+  }
 
-	inline String readCommandName() {
-		_gotoNextToken();
-		_readChar();
-		return _s.readStringUntil(']');
-	}
+  inline String readCommandName() {
+    _gotoNextToken();
+    _readChar();
+    return _s.readStringUntil(']');
+  }
 
-	inline long int readInt() {
-		_gotoNextToken();
-		return Serial.parseInt();
-	}
+  inline long int readInt() {
+    _gotoNextToken();
+    return Serial.parseInt();
+  }
 
-	bool inputAvailable() {
-		return _s.available();
-	}
+  bool inputAvailable() {
+    return _s.available();
+  }
+  
+  inline void gobbleTrailingSpaces() {
+    while (inputAvailable() && isWhitespace(_s.peek())) {
+      int c = _s.read();
+    }
+  }
 
 private:
-		
-	inline void _gotoNextToken() {
-		while (isWhitespace(_s.peek())) {
-			int c = _s.read();
-			if (c == -1) {
-				_n("[ERROR]: Timeout.");
-			}
-		}
-	}
+    
+  inline void _gotoNextToken() {
+    while (isWhitespace(_s.peek())) {
+      int c = _s.read();
+      if (c == -1) {
+        _n("[ERROR]: Timeout.");
+      }
+    }
+  }
 
-	inline void _assertRead(char c) {
-		char r = _s.read();
-		if (c == -1) {
-			_n("[ERROR]: Timeout.");
-		}
-		else if (r != c) {
-			_("[ERROR]: Expected "); _(c); _(" but got "); _(r); _n(" instead.");
-		}
-	}
+  inline void _assertRead(char c) {
+    char r = _s.read();
+    if (c == -1) {
+      _n("[ERROR]: Timeout.");
+    }
+    else if (r != c) {
+      _("[ERROR]: Expected "); _(c); _(" but got "); _(r); _n(" instead.");
+    }
+  }
 
-	inline char _readChar() {
-		int c = _s.read();
-		if (c == -1) {
-			_n("[ERROR]: Timeout");
-		}
-		return c;
-	}
+  inline char _readChar() {
+    int c = _s.read();
+    if (c == -1) {
+      _n("[ERROR]: Timeout");
+    }
+    return c;
+  }
 
-	Stream &_s;
+  Stream &_s;
 };
 
